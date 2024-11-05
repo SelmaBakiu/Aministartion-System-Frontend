d0e00f2849f2adea1d6110a739c3bcf9204eb4af
SelmaBakiu <138330066+SelmaBakiu@users.noreply.github.com>
2024-11-05 12:29:10 +0100

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {AppRoutes} from "./routes";
 import {AppProvider} from "./providers/AppProvider.tsx";
-import './styles/App.css';
-
 
 const App: React.FC = () => {
     return (
